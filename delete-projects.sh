18a4c544486f866f4ad2225815c8c097f878b855
David Puglielli <david.puglielli@dwd.wisconsin.gov>
2022-05-16 08:26:08 -0500

    Extensive changes

---
+++ delete-projects.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source ./vars.sh
+
+echo "$ATTESTOR_PROJECT_ID delete"
+gcloud beta billing projects unlink $ATTESTOR_PROJECT_ID
+gcloud projects delete $ATTESTOR_PROJECT_ID --quiet
+
+echo "$DEPLOYER_PROJECT_ID delete"
+gcloud beta billing projects unlink $DEPLOYER_PROJECT_ID
+gcloud projects delete $DEPLOYER_PROJECT_ID --quiet
+
+echo "$ATTESTATION_PROJECT_ID delete"
+gcloud beta billing projects unlink $ATTESTATION_PROJECT_ID
+gcloud projects delete $ATTESTATION_PROJECT_ID --quiet
